8720c83a51732e9139003e4d2586dbc2ecf544b0
Keerthana-R06 <pushparaju242@gmail.com>
2026-02-10 18:18:12 +0530

    main

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class App{
+    public static void main(String[] args){
+        System.out.println("Maven build executed successfully");
+        System.out.println("Jenkins Poll SCM automation working");
+    }
+}
