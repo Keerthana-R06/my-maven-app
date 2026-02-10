d02d499d25ee994af9b0d17ec9aa0cb4a7e67f94
Keerthana-R06 <pushparaju242@gmail.com>
2026-02-10 18:29:17 +0530

    Testing Poll SCM auto build

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,6 @@ package com.example;
 public class App{
     public static void main(String[] args){
         System.out.println("Maven build executed successfully");
-        System.out.println("Jenkins Poll SCM automation working");
+        System.out.println("Jenkins Poll SCM automation working properly");
     }
 }
